1dd7e914cceab926d02a66e240cdc1489a309efc
Daniel Regeci <536331+ovx@users.noreply.github.com>
2025-10-16 08:04:49 +0900

    v2.0.5

--- altcha.php
+++ altcha.php
@@ -6,8 +6,8 @@
  * Description: ALTCHA for WordPress delivers professional, invisible spam protection that works with any form plugin, handles heavy traffic, and keeps your site safe without annoying visitors. With built-in firewall, rate limiting, and GDPR-compliant security, it’s the all-in-one solution for fast, reliable, and privacy-first WordPress protection.
  * Author: Altcha.org
  * Author URI: https://altcha.org
- * Version: 2.0.4
- * Stable tag: 2.0.4
+ * Version: 2.0.5
+ * Stable tag: 2.0.5
  * Requires at least: 5.0
  * Requires PHP: 7.3
  * Tested up to: 6.8
