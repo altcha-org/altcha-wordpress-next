7b4ab25ad233538f2b800132953c4271bdec2991
Daniel Regeci <536331+ovx@users.noreply.github.com>
2026-03-23 10:50:18 +0800

    fix: auto updater

--- includes/updater.php
+++ includes/updater.php
@@ -36,18 +36,30 @@ class AltchaPluginUpdater
     $this->authorize_token = $token;
     add_filter("pre_set_site_transient_update_plugins", array($this, "check_github_updates"));
     add_filter("upgrader_post_install", array($this, "post_install"), 10, 3);
+    add_filter("http_request_args", array($this, "exclude_from_wporg"), 10, 2);
   }
 
   public function check_github_updates($transient)
   {
-    if (empty($transient->checked)) {
+    if (!is_object($transient)) {
       return $transient;
     }
+
+    if (!isset($transient->checked) || !is_array($transient->checked)) {
+      return $transient;
+    }
+
+    if (!isset($transient->checked[$this->plugin_slug])) {
+      return $transient;
+    }
+
     $github_data = $this->get_github_data();
-    if (is_array($github_data)) {
+
+
+    if (is_array($github_data) && !empty($github_data["tag_name"])) {
       $tag = $github_data["tag_name"];
       $new_version = preg_replace("/^v/", "", $tag);
-      if ($github_data && version_compare($transient->checked[$this->plugin_slug], $new_version, "<")) {
+      if (version_compare($transient->checked[$this->plugin_slug], $new_version, "<")) {
         $plugin_data = array(
           "new_version" => $new_version,
           "url" => $github_data["html_url"],
@@ -64,6 +76,8 @@ class AltchaPluginUpdater
         $obj->package = $plugin_data["package"];
         $obj->plugin = $this->plugin_slug;
         $transient->response[$this->plugin_slug] = $obj;
+      } else {
+        unset($transient->response[$this->plugin_slug]);
       }
     }
     return $transient;
@@ -78,7 +92,7 @@ class AltchaPluginUpdater
       )
     );
     if ($this->authorize_token) {
-      $args["headers"]["Authorization"] = "token {$this->authorize_token}";
+      $args["headers"]["Authorization"] = "Bearer {$this->authorize_token}";
     }
     $response = wp_remote_get($url, $args);
     if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
@@ -89,6 +103,10 @@ class AltchaPluginUpdater
 
   public function post_install($response, $hook_extra, $result)
   {
+    if (!isset($hook_extra['plugin']) || $hook_extra['plugin'] !== $this->plugin_slug) {
+      return $result;
+    }
+
     global $wp_filesystem;
     $install_directory = plugin_dir_path($this->plugin_file);
     $wp_filesystem->move($result["destination"], $install_directory);
@@ -97,5 +115,17 @@ class AltchaPluginUpdater
       activate_plugin($this->plugin_slug);
     }
     return $result;
+  }
+
+  public function exclude_from_wporg($args, $url)
+  {
+    if (strpos($url, "api.wordpress.org/plugins/update-check") !== false) {
+      $plugins = json_decode($args["body"]["plugins"], true);
+      if (isset($plugins["plugins"][$this->plugin_slug])) {
+        unset($plugins["plugins"][$this->plugin_slug]);
+        $args["body"]["plugins"] = json_encode($plugins);
+      }
+    }
+    return $args;
   }
 }
